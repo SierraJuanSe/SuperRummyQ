a711a4f0fc572cd1dd771706450a145463eb7cae
SierraJuanSe <juansebastiansierrac@gmail.com>
2020-04-29 22:39:34 -0500

    Avance en el modelo

--- Servidor/src/main/java/BackEnd/main.java
+++ Servidor/src/main/java/BackEnd/main.java
@@ -1,12 +1,12 @@
 package BackEnd;
 
 import java.net.UnknownHostException;
-
 import utilidades.ServerConn;
 
 class main {
 
 	public static void main(String[] args) {
+		
 		
 		try {
 			ServerConn server = new ServerConn();
--- Servidor/src/main/java/modelo/Tablero.java
+++ Servidor/src/main/java/modelo/Tablero.java
@@ -1,5 +1,26 @@
 package modelo;
 
-public class Tablero {
+import java.util.ArrayList;
 
+public class Tablero {
+	private ArrayList<Jugada> jugadas;
+
+	public Tablero() {
+		this.jugadas = new ArrayList<Jugada>();
+	}
+	
+	public void insertJugada(String id, ArrayList<Ficha> fichas) {
+		
+	}
+
+	public ArrayList<Jugada> getJugadas() {
+		return jugadas;
+	}
+
+	public void setJugadas(ArrayList<Jugada> jugadas) {
+		this.jugadas = jugadas;
+	}
+	
+	
+	
 }
