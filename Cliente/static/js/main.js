b9fc8e8f0d4aa9b3145372363a120affd4ffe4f2
Oscar <oscar.reyes01@correo.usa.edu.co>
2020-05-01 18:48:52 -0500

    main.js editado

--- Cliente/static/js/main.js
+++ Cliente/static/js/main.js
@@ -1,9 +1,42 @@
-/**
- * Script principal de la aplicacion
- */
+(function(){
+var usuario = '';
+var wsUri = "ws://localhost:30001";
+var websocket = new WebSocket(wsUri);
 
 $('#unir').click(function () {
+	login();
+
+});
+
+function login(){
+
+	usuario = $('#first_name2').val();
+	registro(usuario);
     $(location).attr('href',"Tablero.html");
       $('#tablero').show(1000);
-  });
+      alert("Nombre jugador: "+ usuario.toUpperCase(usuario));
+  };
 
+function registro(usuario) {
+	registro = {
+		type : 'registro',
+		nombre : usuario
+	};
+
+	
+}
+
+
+
+ $('#iniciar').click(function jugada(){
+
+	registro = {
+		type : 'iniciar',
+		nombre : usuario
+
+	};
+ }); 
+
+
+
+}())
