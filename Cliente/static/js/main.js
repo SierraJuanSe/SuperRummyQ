6243719cd2b91c9462620152c132a58157734799
Oscar <oscar.reyes01@correo.usa.edu.co>
2020-05-02 09:21:15 -0500

    Revision main.js

--- Cliente/static/js/main.js
+++ Cliente/static/js/main.js
@@ -1,7 +1,17 @@
 (function(){
-var usuario = '';
 var wsUri = "ws://localhost:30001";
 var websocket = new WebSocket(wsUri);
+var jugador='';
+
+
+websocket.onopen = function(evt) { 
+    console.log("--Conectado--"); 
+};
+
+
+websocket.onerror = e =>{
+	console.log(e.data);
+};
 
 $('#unir').click(function () {
 	login();
@@ -10,21 +20,30 @@ $('#unir').click(function () {
 
 function login(){
 
-	usuario = $('#first_name2').val();
-	registro(usuario);
+	var nombre = $('#first_name2').val();
+	registro(nombre);
     $(location).attr('href',"Tablero.html");
       $('#tablero').show(1000);
-      alert("Nombre jugador: "+ usuario.toUpperCase(usuario));
+      alert("Nombre jugador: "+ nombre.toUpperCase(nombre));
   };
 
-function registro(usuario) {
+function registro(nombre) {
 	registro = {
 		type : 'registro',
-		nombre : usuario
+		nombre : nombre
 	};
+	jugador=JSON.stringify(registro);
+	websocket.send(jugador);
+	alert(jugador);
+}
 
+
+
+function setTurno() {
 	
 }
+
+
 
 
 
