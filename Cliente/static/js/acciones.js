79be244f96041a12dd95cb2b673eb73988206864
Oscar <oscar.reyes01@correo.usa.edu.co>
2020-05-03 21:51:28 -0500

    Subo algo

--- Cliente/static/js/acciones.js
+++ Cliente/static/js/acciones.js
@@ -19,6 +19,7 @@ function login(jugador) {
 }
 
 function registroJugador(mes) {
+	console.log(mes);
   if (mes.jugador == nombre) {
     rol = mes.rol;
 		numfichas = 14;
@@ -35,6 +36,7 @@ function registroJugador(mes) {
 
 		}
   }
+
 }
 
 function iniciar(mes) {
@@ -60,11 +62,44 @@ function nuevaFicha(mes) {
   mostrarFichas(nuevaFicha);
 }
 
-function Turno(mes) {
-  if (mes.turno == Turno) {
-    //habilitar botones para jugar
 
-  } else {
-    //deshabilitar botones para juegar
-  }
+
+function turno(turno){  //ENTRA EL DATO TIPO ENTERO TURNO QUE DEBE MANEJAR EL SERVER
+	if(jugador.turno==turno){
+		//ACTIVE EL BOTON DE PEDIR CARTA Y HABILITE EL BOTON DE JUGAR CARTAS
+	}else{
+		//MANTENGA LOS BOTONES DESHABILITADOS
+	}
+
+
+
+function setInfojugador(infoJugador){ //SE MNECESITA QUE INGRESE EL OBJETO JUGADOR QUE LO ENVIARA EL SERVER CUANDO SE CREE O SE UNA ALGUIEN,
+	                                  //ARRIBA SE DECLARO LA VARIABLE JUGADOR QUE SERA GLOBAL Y PERTENECERA A CADA CLIENTE
+	jugador={
+		nombre:infoJugador.nombre,
+		puntaje:infoJugador.puntaje,
+		numFichas:infoJugador.numFichas,
+		turno:infoJugador.turno,
+	};
 }
+
+
+function publicarStatusJugador(){  //ESTE METODO SE DEBE EJECUTAR CADA VEZ QUE EL JUGADOR TERMINE SU TURNO
+	status={
+		infoJugador:jugador,
+		type:"status"
+	};
+	var msm=JSON.stringify(status);
+	sendCard(msm);   //ESTE ES EL METODO QUE ENVIA LOS DATOS AL SERVER
+}
+
+
+//------------------------------------------------------------------------------------------------------------
+
+
+
+function ingresarJugadores(jugadorRecibido){
+	//ENTRA UN OBJETO JUGADOR,Y SE LE ASIGNA A LA ULTIMA POSICION DEL ARRAY JUGADORES
+	jugadores.push(jugadorRecibido);
+}
+
