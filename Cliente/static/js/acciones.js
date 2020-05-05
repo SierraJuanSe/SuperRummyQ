83f23209f0a8031e5759497435f45f8be45c9595
Your Name <you@example.com>
2020-05-04 21:39:15 -0500

    Ya se envian las cartas

--- Cliente/static/js/acciones.js
+++ Cliente/static/js/acciones.js
@@ -4,6 +4,11 @@ var rol = '';
 var numfichas = 0;
 var mifichas = new Array();
 var otrosjugadores = new Array();
+var jugada = {
+  type : "jugada",
+  fichas : []
+};
+
 
 function login(jugador) {
   nombre = jugador;
@@ -17,6 +22,38 @@ function login(jugador) {
   websocket.send(nombrejugador);
   //alert(nombrejugador);
 }
+
+function agregarJugada(id, position) {
+  ficha = {id: id, espacio: position};
+  jugada.fichas.forEach((item, i) => {
+    if(item.id == id){
+      jugada.fichas.splice(i, 1);
+    }
+  });
+
+  jugada.fichas.push(ficha);
+  // console.log(jugada.fichas);
+}
+
+function enviarJugada() {
+  var mensaje=JSON.stringify(jugada);
+ console.log(mensaje);
+  websocket.send(mensaje);
+}
+
+function dibujarJugada(mes) {
+  for (let i = 0; i < mes.fichas.length; i++) {
+    $('#'+mes.fichas[i].id ).removeAttr('style');
+       $('#'+mes.fichas[i].espacio).append(document.getElementById(mes.fichas[i].id));
+      // console.log($('#'+mes.fichas[i].id));
+      // console.log($('#'+mes.fichas[i].espacio));
+
+    //  console.log(mes.fichas[i].espacio);
+    //  console.log(mes.fichas[i].id);
+  }
+
+}
+
 
 function registroJugador(mes) {
 
@@ -61,8 +98,8 @@ function iniciar(mes) {
 function pintarNombres() {
   for (let i = 0; i < otrosjugadores.length; i++) {
 
-    $('#u'+(i+1)).empty().append(otrosjugadores[i].nombre);
-    $('#nf'+(i+1)).empty().append(otrosjugadores[i].numfichas);
+    $('#u' + (i + 1)).empty().append(otrosjugadores[i].nombre);
+    $('#nf' + (i + 1)).empty().append(otrosjugadores[i].numfichas);
   }
 }
 
@@ -82,56 +119,116 @@ function mostrarFichas(mifichas) {
 
   for (let i = 0; i < mifichas.length; i++) {
 
-    console.log(mifichas[i].id);
+    // console.log(mifichas[i].id);
     llevardrop(mifichas[i].id);
     // $('#'+m).attr('draggable',false);
 
   }
+
 }
 
 function robo(mes) {
 
-for (let i = 0; i < otrosjugadores.length; i++) {
-     if (mes.jugador==otrosjugadores[i].nombre) {
+  for (let i = 0; i < otrosjugadores.length; i++) {
+    if (mes.jugador == otrosjugadores[i].nombre) {
 
-       otrosjugadores[i].numfichas=mes.numFichas;
-     }
-}
-pintarNombres();
+      otrosjugadores[i].numfichas = mes.numFichas;
+    }
+  }
+  pintarNombres();
 }
 
 
 function nuevaFicha(mes) {
   nueva = mes.ficha;
   console.log(mes);
-  numfichas=mes.numFichas;
+  numfichas = mes.numFichas;
   mostrarFichas(nueva);
   $('#nfichasusuario').empty().append(numfichas);
 }
 
 function funTurno(mes) {
+  var tipo='';
   if (mes.jugador == nombre) {
+    habilitarFichas('hab');
     //habilitar botones para jugar
-    console.log('Es mi turno'+mes.jugador);
-     $('#jugar').prop('disabled', false);
-     $('#robar').prop('disabled', false);
-  } else {
-    //deshabilitar botones para jugar
-    console.log('Es el turrno de '+mes.jugador);
-    $('#robar').prop('disabled', true);
-    $('#jugar').prop('disabled', true);
-  }
-}
+    console.log('Es mi turno' + mes.jugador);
+    $('#pinturno').css('top', '500px');
+    $('#pinturno').show();
 
+    $('#jugar').show(1000);
+    $('#robar').prop('disabled', false);
+    $('#pinturno1').hide();
+    $('#pinturno2').hide();
+    $('#pinturno3').hide();
+  } else {
+    habilitarFichas('des');
+    //deshabilitar botones para jugar
+    $('#pinturno').hide();
+
+    // $('#pinturno').	css('top','14px');
+    // $('#pinturno').show();
+    for (let i = 0; i < otrosjugadores.length; i++) {
+
+      if (mes.jugador == otrosjugadores[i].nombre) {       
+        $('#pinturno'+(i+1)).show();
+
+    }else{
+      $('#pinturno'+(i+1)).hide();
+    }
+  }
+
+     console.log('Es el turrno de ' + mes.jugador);
+    $('#robar').prop('disabled', true);
+    $('#jugar').hide(1000);
+
+  }
+
+}
 
 function enviarTurno() {
   turno = {
     type: 'pasar',
     nombre: 'nombre',
-    robo:true
+    robo: true
   }
 
   mensaje = JSON.stringify(turno);
   websocket.send(mensaje);
 
 }
+
+function habilitarFichas(tipo) {
+  var col = '';
+  var nombre = '';
+  var ncarta = '';
+  for (var j = 1; j <= 4; j++) {
+    if (j == 1) {
+      col = 'a';
+    } else if (j == 2) {
+      col = 'r';
+    } else if (j == 3) {
+      col = 'n';
+    } else {
+      col = 'y';
+    }
+    for (var i = 1; i <= 13; i++) {
+      nombre = col + i;
+      for (let j = 1; j <= 2; j++) {
+        if (j == 1) {
+          ncarta = 'a'
+        } else {
+          ncarta = 'b'
+        }
+         if (tipo=='hab') {
+          $('#'+nombre+ncarta).attr('draggable',true);
+         }else{
+          $('#'+nombre+ncarta).attr('draggable',false);
+         }
+        
+
+      }
+    }
+  }
+}
+
